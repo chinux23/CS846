136cf449036b484c43e1f856bf97efeb5b794d29
Chen Huang <chen_huang@apple.com>
2017-12-08 21:15:38 -0500

    Update.

--- Predict.py
+++ Predict.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!python -u
 
 import APIREC
 import Aggregator
@@ -6,6 +6,7 @@ import Mining
 import math
 from Queue import PriorityQueue, heapq
 from multiprocessing import Pool
+from functools import partial
 import time
 
 InTraining = False
@@ -58,29 +59,31 @@ def predict(total_number):
     print("Final prediction accuracy: " + str(1.0 * hit / total_number))
 
 def _predict(n, training=[0.5]):
+    # print("{} with {}".format(n, training))
+
     hit = 0
     target = APIREC.target_context[n][0]
     change_context = APIREC.target_context[n][1]
     code_context = APIREC.target_context[n][2]
 
     h = []
-    for i in len(training):
+    for i in training:
         h.append([])
 
     for candidate in APIREC.P_list:
 
         candidate_target = dict(target)
         candidate_target['label'] = candidate[2]
-        change_score = APIREC.change_score_transaction(target, change_context)
-        code_score = APIREC.code_score_transaction(target, code_context)
-        for i in len(training):
+        change_score = APIREC.change_score_transaction(candidate_target, change_context)
+        code_score = APIREC.code_score_transaction(candidate_target, code_context)
+        for i in range(len(training)):
             w_change = training[i]
             w_code = 1 - w_change
             _score = w_change * change_score + w_code * code_score
             heapq.heappush(h[i], (_score, candidate))
 
-    hit = []
-    for i in len(training):
+    hit = [0] * len(training)
+    for i in range(len(training)):
         top10 = heapq.nlargest(10, h[i])
         # print("Top 10: " + str(top10))
         prediction = Mining.atmoic_change(target)
@@ -90,8 +93,9 @@ def _predict(n, training=[0.5]):
             candidate = item[1]
             if candidate == prediction:
                 hit[i] += 1
-                # print("hit")
+                # print("{} hit".format(i))
                 break
+    # print("return {}".format(hit))
     return hit
 
 def main():
@@ -101,21 +105,24 @@ def main():
 
     t1 = time.time()
 
-    size = len(APIREC.target_context)
-    # size = 10000
+    # size = len(APIREC.target_context)
+    size = 1000
+
     print("Processing " + str(size) + " items.")
     p = Pool()
 
-    result = p.map(_predict, range(size))
+    weights = [0.2, 0.5, 0.9]
+
+    result = p.map(partial(_predict, training=weights), range(size))
 
     p.close()
     p.join()
 
     print("Took " + str(time.time() - t1) + " seconds.")
-
-    for i in len(result):
-        print("Total evaluation: " + str(len(result[i])))
-        print("Accurracy {} is ".format(i) + str(sum(result[i]) * 1.0 / size))
+    print("Total evaluation: " + str(len(result)))
+    result = [sum(x) for x in zip(*result)]
+    for i in range(len(result)):
+        print("Accurracy {} is ".format(i) + str(result[i] * 1.0 / size))
 
 if __name__ == "__main__":
     main()
